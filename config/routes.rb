6ce0b45398a372612a5b41ec91e59054c5012b05
Dyo Medio <jsp.dio@gmail.com>
2015-07-02 15:30:53 +0700

    switch to dev view

--- config/routes.rb
+++ config/routes.rb
@@ -21,9 +21,9 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'home#index'
-  # root 'undev#index'
+  root 'undev#index'
   # Switcher <--
-  root 'home#index'
+  # root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
